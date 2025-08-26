e06e4027cdc457c90c78514436d18e9c70eff194
joeln4 <norlingjoel4@gmail.com>
2025-08-26 18:16:05 +0200

    li till divs

--- frontend/src/Components/BookingCard/BookingCard.tsx
+++ frontend/src/Components/BookingCard/BookingCard.tsx
@@ -5,14 +5,13 @@ type Props = {}
 
 const BookingCard = (props: Props) => {
   return <div className="booking-card">
-
-        <ul className="bc-content">
-            <li className="bc-id">1001</li>
-            <li className="bc-appointment-time">2025-08-26 14:30</li>
-            <li className="bc-treatments">Klippning</li>
-            <li className="bc-customer">Joel@gmail.com</li>
-            <li className="bc-status">Bokad</li>
-        </ul>
+        <div className="bc-content">
+            <div className="bc-id">1001</div>
+            <div className="bc-appointment-time">2025-08-26 14:30</div>
+            <div className="bc-treatments">Klippning</div>
+            <div className="bc-customer">Joel@gmail.com</div>
+            <div className="bc-status">Bokad</div>
+        </div>       
     </div>
 };
 
