f3e5f6a6a967487d8063fa5a027fd1cc4a2610c7
joeln4 <norlingjoel4@gmail.com>
2025-09-11 22:56:19 +0200

    hämtaledigatider på god väg

--- api/Services/BookingAvailability.cs
+++ api/Services/BookingAvailability.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Booking;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Services
 {
@@ -16,17 +17,54 @@ namespace api.Services
             _context = context;
         }
 
-        public string[] GetAvailableTimes(BookingDateDto dateDto)
+        public async Task<string[]> GetAvailableTimes(BookingDateDto dateDto)
         {
             var bookingTimes = new[] { "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00" };
 
+            //Ska ändras så att det blir egen entitet och tabell i databasen, så att admin kan ändra
+            var openingTime = new TimeOnly(9, 0);
+            var closingTime = new TimeOnly(18, 0);
+
+            
+            //Parsear det inkomna datumet från string yyyy-MM-dd till DateOnly
             if (!DateOnly.TryParseExact(dateDto.Date, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dateValue))
             {
                 throw new("Ogiligt datumformat, ska vara yyyy-MM-dd");
             }
 
+            //Sätter intervall för dagen för att kunna hämta bokningar satta samma dag
+            var startOfDay = dateValue.ToDateTime(openingTime);
+            var endOfDay = dateValue.ToDateTime(closingTime);
 
-            
+            var bookingsThatDay = await _context.Bookings.AsNoTracking()
+            .Where(b => b.StartTime >= startOfDay && b.StartTime < endOfDay)
+            .Select(b => new { b.StartTime, b.EndTime }).ToListAsync();
+
+            //Görs till ticks (heltal) för att kunna summeras i databasen
+            var totalTicks = await _context.Treatments.Where(t => dateDto.TreatmentIds.Contains(t.Id))
+            .SumAsync(t => t.Duration.Ticks);
+
+            var totalDuration = TimeSpan.FromTicks(totalTicks);
+
+            var slotStart = startOfDay;
+            var slotEnd = slotStart + totalDuration;
+
+            //Steg som kollar om ledig plats finns var 15e minut på dagen för att det är den kortaste behandlingen
+            TimeSpan slotStep = TimeSpan.FromMinutes(15);
+
+
+            var availableTimes = new List<string>();
+
+            foreach (var b in bookingsThatDay)
+            {
+                if (slotStart >= b.EndTime && slotEnd <= b.StartTime)
+                {
+                    // availableTimes.Add(slotStart);
+                }
+                
+            }
+
+    
             return bookingTimes;
         }
     }
