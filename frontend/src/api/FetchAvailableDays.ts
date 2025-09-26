d692c4c8c89ab06bc62fa1e7f6d72c0b137e525d
joeln4 <norlingjoel4@gmail.com>
2025-09-26 17:53:09 +0200

    Update FetchAvailableDays.ts

--- frontend/src/api/FetchAvailableDays.ts
+++ frontend/src/api/FetchAvailableDays.ts
@@ -1,16 +1,4 @@
-async function FetchAvailableDays (
-  date: string,
-  treatmentIds: number[]
-): Promise<string[]> {
-  const res = await fetch("http://localhost:5296/api/booking/times", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ date: date, treatmentIds: treatmentIds }),
-  });
-  if (!res.ok) {
-    throw new Error(`API error: ${res.status} ${res.statusText}`); // Ändra denna?
-  }
-  return res.json();
+async function FetchAvailableDays (){
 }
 
 export default FetchAvailableDays;
