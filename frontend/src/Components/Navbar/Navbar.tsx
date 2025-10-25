040e1fec0b13940bf16fac04ed63bb41ba975135
joeln4 <153519118+joeln4@users.noreply.github.com>
2025-10-25 17:14:20 +0200

    navbar fix

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,22 +1,30 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <a href="/" className="site-title">
-        <img id="nav-logo" src="/images/Hårmixarna-logo.png" alt="logo"/>
-      </a>
-      <ul>
-        <li>
-          <a href="/about">Om oss</a>
-        </li>
-        <li>
-          <a href="/contact">Kontakt</a>
-        </li>
-        <li>
-          <a href="/booking">Boka</a>
-        </li>
-      </ul>
+    <nav>
+      <div id="nav-logo">
+        <a href="/">
+          <img src="/images/Hårmixarna-logo.png" alt="logo" />
+        </a>
+      </div>
+      <label htmlFor="menu-toggle" id="menu-icon">
+        &#9776;
+      </label>
+      <input type="checkbox" id="menu-toggle"></input>
+      <div id="nav-menu">
+        <ul id="menu-list">
+          <li>
+            <a href="/about">Om oss</a>
+          </li>
+          <li>
+            <a href="/contact">Kontakt</a>
+          </li>
+          <li>
+            <a href="/booking">Boka</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
